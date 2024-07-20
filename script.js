7157f6926b020275c1ac4a8f1e55dbe69e009543
Coach_1337 <46282057+Coach1337@users.noreply.github.com>
2024-07-20 20:40:12 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+(() => {
+  const url = new URL(window.location)
+  const vid = url.searchParams.get('v') ?? 'NAo38Q9c4xA'
+  url.searchParams.delete('v')
+  const embedUrl = new URL(`https://www.youtube.com/embed/${vid}?${url.searchParams.toString()}`)
+  document.querySelector('iframe').src = embedUrl.href
+})()
